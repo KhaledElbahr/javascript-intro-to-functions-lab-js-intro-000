2378133099cef3c662b060b44e8c24bf4e60ef63
Khaled Elbahr <khaledelbahr2016@gmail.com>
2018-11-24 19:25:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function logWhisper (string) {
 }
 
 function sayHiToGrandma(string) {
-  if(whisper(string) === string){
+  if(string.toLowerCase() === string){
   return "I can't hear you!";
  }
 }
 
 function sayHiToGrandma(string) {
-  if(shout(string) === string){
+  if(string.toUpperCase() === string){
   return "YES INDEED!";
  }
 }
